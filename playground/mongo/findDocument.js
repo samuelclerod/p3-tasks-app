ff23361200fb00e2d92d44329984eefe4b259405
Samuel Rodrigues <samuelclerod@gmail.com>
2021-10-26 20:10:34 -0300

    change methos to promise

--- playground/mongo/findDocument.js
+++ playground/mongo/findDocument.js
@@ -1,24 +1,37 @@
-const { ObjectID } = require("bson");
+const { ObjectID } = require("mongodb");
 const mongodb = require("mongodb");
 const { MongoClient } = mongodb;
 
 const database = "task-manager";
 const connectionURL = "mongodb://localhost:27017";
 
-MongoClient.connect(
-  connectionURL,
-  { useNewUrlParser: true },
-  (error, client) => {
-    if (error) {
-      return console.log("unable to connect to mongodb");
-    }
-    const db = client.db(database);
-    db.collection("users").findOne(
-      { _id: ObjectID("617859a49b8bad2712678c6e") },
-      (err, user) => {
-        if (err) return console.log("Cannot find the user");
-        console.log(user);
-      }
-    );
-  }
-);
+MongoClient.connect(connectionURL, { useNewUrlParser: true }).then((client) => {
+  client
+    .db(database)
+    .collection("users")
+    .find({ age: "16" })
+    .count()
+    .then((count) => console.log("number of regs: ", count));
+
+  client
+    .db(database)
+    .collection("users")
+    .find({ age: "16" })
+    .toArray()
+    .then((arr) => console.log(arr));
+});
+
+// MongoClient.connect(
+//   connectionURL,
+//   { useNewUrlParser: true },
+//   (error, client) => {
+//     if (error) {
+//       return console.log("unable to connect to mongodb");
+//     }
+//     const db = client.db(database);
+//     db.collection("users").findOne({ age: "16" }, (err, user) => {
+//       if (err) return console.log("Cannot connect to collection");
+//       console.log(user);
+//     });
+//   }
+// );
--- playground/mongo/insertTasks.js
+++ playground/mongo/insertTasks.js
@@ -1,26 +1,35 @@
-const mongodb = require('mongodb');
-const {MongoClient} = mongodb;
-
+const mongodb = require("mongodb");
+const { MongoClient } = mongodb;
 
 const database = "task-manager";
 const connectionURL = "mongodb://localhost:27017";
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
-  if(error) return console.log('unable to connect to mongodb');
-  const db = client.db(database);
 
-  db.collection('tasks').insertMany([
-    {
-      description: 'First description', completed:true,
-    },
-    {
-      description: 'Second description', completed:true,
-    },
-    {
-      description: 'Third description', completed:false,
-    },
-  ], (error, result)=> {
-    if (error) return console.log("Unable to insert documents");
-    console.log(result);
-  })
+const tasks = [
+  {
+    description: "Vir para unijuazeiro assistir aula",
+    completed: true,
+  },
+  {
+    description: "Fazer feira no atacadão",
+    completed: false,
+  },
+  {
+    description: "Comprar um novo macbook pro quando juntar os 18K",
+    completed: false,
+  },
+];
 
+MongoClient.connect(connectionURL, {
+  useNewUrlParser: true,
 })
+  .then((client) => {
+    client
+      .db(database)
+      .collection("tasks")
+      .insertMany(tasks)
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error));
+  })
+  .catch((error) => {
+    console.log("Unable do connect to mongodb");
+  });
