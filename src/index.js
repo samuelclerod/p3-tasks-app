ee0070adfcf48c144f24fb3954633f30f37ce092
Samuel Rodrigues <samuelclerod@gmail.com>
2021-11-16 19:23:36 -0300

    feat: refator to users routes

--- src/index.js
+++ src/index.js
@@ -1,42 +1,42 @@
+const { response, query } = require("express");
 const express = require("express");
 require("./db/mongoose");
 
 const Task = require("./models/Task");
-const User = require("./models/User");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const usersRoutes = require('./routes/usersRoutes');
+
 app.use(express.json());
+app.use(usersRoutes)
 
-app.post("/users", (request, response) => {
-  const user = new User(request.body);
-  user
+
+app.post("/tasks", (request, response) => {
+  const task = new Task(request.body);
+  task
     .save()
-    .then(() => {
-      response.status(201).send(user);
-    })
-    .catch((error) => {
-      response.status(400).send({
-        error: error.message,
-      });
-    });
-});
-
-app.get("/users", (request, response) => {
-  User.find()
-    .then((users) => {
-      response.status(200).send(users);
-    })
-    .catch((error) => {
-      response.status(500).send();
-    });
+    .then(() => response.status(201).send(task))
+    .catch((error) => response.status(400).send(error.message));
 });
 
 app.get("/tasks", (request, response) => {
   Task.find({})
     .then((tasks) => {
       response.send(tasks);
+    })
+    .catch((error) => {
+      response.status(500).send();
+    });
+});
+
+app.get("/tasks/:id", (request, response) => {
+  const id = request.params.id;
+  Task.findById(id)
+    .then((task) => {
+      if (task) response.send(task);
+      response.status(404).send();
     })
     .catch((error) => {
       response.status(500).send();
